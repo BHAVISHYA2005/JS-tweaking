d043aaeccf16ffeec90f742105de3ae10741e83d
Bhavishya <handballaggarwal@gmail.com>
2025-01-01 22:12:52 +0530

    tunings

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Lock in , we dont have time")
+
+// var a = 9;
+// var b =3;
+// var x ="reporting from lambot no. 98 "
+// console.log(a + b + 69)
+
+// let a = 56
+// let b = "namaste"
+// let x = 76.76
+// const p ="lambot no. 98"
+// console.log(a,b,x,p)
+// console.log(typeof a + typeof b + typeof x + typeof p) 
+var o = {
+    name:"bhavishya",
+    age:19,
+    heightplace: 5.8 & "delshi",
+}
+console.log(o)
---
+++ vedio 56/script.js
@@ -0,0 +1,6 @@
+console.log("Conditional opearators in javascript")
+
+let a = 9
+let b = 7
+
+let c = a > b ? (a - b) : (b - a);
